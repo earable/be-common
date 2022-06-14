f94cc6d683d6006acc4843cd1a5b90f9fe52a322
BinhNH <98414325+BinhLuka@users.noreply.github.com>
2022-06-14 21:42:49 +0700

    add handle reactive security context holder

--- jwt-webflux/src/main/java/ai/earable/platform/common/jwt/JwtUtils.java
+++ jwt-webflux/src/main/java/ai/earable/platform/common/jwt/JwtUtils.java
@@ -1,8 +1,16 @@
 package ai.earable.platform.common.jwt;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
 
 import java.io.*;
 import java.security.KeyFactory;
@@ -11,20 +19,23 @@ import java.security.PublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
+import java.util.Collection;
+import java.util.Date;
 import java.util.Properties;
 
+@Component
 @Slf4j
 public class JwtUtils {
-    private static final JwtUtils INSTANCE = new JwtUtils();
+    private static final String AUTHORITIES_KEY = "role"; //Need to map to IMS
+    private final PublicKey publicKey;
 
-    private static PublicKey publicKey;
-
-    private JwtUtils() {
+    //TODO: Clean this method
+    public JwtUtils() {
         log.info("Loading authentication public key");
         final Properties properties = new Properties();
         String publicKeyFilePath = "";
-        try (final InputStream stream =
-                     this.getClass().getClassLoader().getResourceAsStream("application.properties")) {
+        try (final InputStream stream = this.getClass()
+                .getClassLoader().getResourceAsStream("application.properties")) {
             properties.load(stream);
             publicKeyFilePath = (String) properties.get("earable.auth.public-key-file-path");
 
@@ -62,15 +73,29 @@ public class JwtUtils {
         }
     }
 
-    public static JwtUtils getInstance() {
-        return INSTANCE;
+    public Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser().setSigningKey(publicKey).parseClaimsJws(token).getBody();
     }
 
-    public Claims decode(String jwt) throws JWTParseException {
-        try {
-            return Jwts.parser().setSigningKey(publicKey).parseClaimsJws(jwt).getBody();
-        } catch (Exception e) {
-            throw new JWTParseException("Token is invalid!");
-        }
+    public boolean validateToken(String token) {
+        return !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        final Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date());
+    }
+
+    public Date getExpirationDateFromToken(String token) {
+        return getAllClaimsFromToken(token).getExpiration();
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = getAllClaimsFromToken(token);
+        Object authoritiesClaim = claims.get(AUTHORITIES_KEY);
+        Collection<? extends GrantedAuthority> authorities = authoritiesClaim == null ? AuthorityUtils.NO_AUTHORITIES
+            : AuthorityUtils.commaSeparatedStringToAuthorityList(authoritiesClaim.toString());
+        User principal = new User(claims.getSubject(), "", authorities);
+        return new UsernamePasswordAuthenticationToken(principal, token, authorities);
     }
 }
