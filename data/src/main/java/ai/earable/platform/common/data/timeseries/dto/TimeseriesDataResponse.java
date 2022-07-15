f07bb89bde4c00d66500811d488be6ad3c69ef77
BinhNH <98414325+BinhLuka@users.noreply.github.com>
2022-07-15 16:47:58 +0700

    remove get method

--- data/src/main/java/ai/earable/platform/common/data/timeseries/dto/TimeseriesDataResponse.java
+++ data/src/main/java/ai/earable/platform/common/data/timeseries/dto/TimeseriesDataResponse.java
@@ -1,14 +1,9 @@
 package ai.earable.platform.common.data.timeseries.dto;
 
 import ai.earable.platform.common.data.timeseries.model.Data;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.codehaus.jackson.annotate.JsonIgnore;
-
-import java.util.List;
-import java.util.Map;
 
 /**
  * Created by BinhNH on 3/17/22
@@ -19,19 +14,4 @@ import java.util.Map;
 public class TimeseriesDataResponse {
     private String status;
     private Data data;
-
-    @JsonIgnoreProperties
-    public Map<String, String> getTags(){
-        return data.getResult().get(0).getMetric();
-    }
-
-    @JsonIgnoreProperties
-    public List<String> getVectorValue(){
-        return data.getResult().get(0).getValue();
-    }
-
-    @JsonIgnoreProperties
-    public List<List<String>> getMatrixValues(){
-        return data.getResult().get(0).getValues();
-    }
 }
