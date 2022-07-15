61dc486f28d4d762d3a9b74df7e1deb089abd8ad
BinhNH <98414325+BinhLuka@users.noreply.github.com>
2022-07-15 15:22:47 +0700

    add json ignored for get method

--- data/src/main/java/ai/earable/platform/common/data/timeseries/dto/TimeseriesDataResponse.java
+++ data/src/main/java/ai/earable/platform/common/data/timeseries/dto/TimeseriesDataResponse.java
@@ -4,6 +4,7 @@ import ai.earable.platform.common.data.timeseries.model.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.codehaus.jackson.annotate.JsonIgnore;
 
 import java.util.List;
 import java.util.Map;
@@ -18,14 +19,17 @@ public class TimeseriesDataResponse {
     private String status;
     private Data data;
 
+    @JsonIgnore
     public Map<String, String> getTags(){
         return data.getResult().get(0).getMetric();
     }
 
+    @JsonIgnore
     public List<String> getVectorValue(){
         return data.getResult().get(0).getValue();
     }
 
+    @JsonIgnore
     public List<List<String>> getMatrixValues(){
         return data.getResult().get(0).getValues();
     }
