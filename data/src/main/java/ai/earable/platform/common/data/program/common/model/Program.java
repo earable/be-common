339fe055eeb528084fdcf945de1e290635f75ba7
BinhNH <binhnh@earable.ai>
2022-08-08 12:14:32 +0700

    update model

--- data/src/main/java/ai/earable/platform/common/data/program/common/model/Program.java
+++ data/src/main/java/ai/earable/platform/common/data/program/common/model/Program.java
@@ -1,7 +1,14 @@
 package ai.earable.platform.common.data.program.common.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * Created by BinhNH on 08/08/2022
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Program {
 }
