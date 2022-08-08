c56281f2dd9d28c459aa2c45c00176c66f8d6fcc
BinhNH <binhnh@earable.ai>
2022-08-08 18:29:43 +0700

    add @Data to UserTask

--- data/src/main/java/ai/earable/platform/common/data/program/user/model/UserTask.java
+++ data/src/main/java/ai/earable/platform/common/data/program/user/model/UserTask.java
@@ -1,12 +1,10 @@
 package ai.earable.platform.common.data.program.user.model;
 
 import ai.earable.platform.common.data.program.common.model.Task;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
-import lombok.With;
+import lombok.*;
 import org.springframework.data.cassandra.core.mapping.UserDefinedType;
 
+@Data
 @Builder
 @UserDefinedType
 @NoArgsConstructor
