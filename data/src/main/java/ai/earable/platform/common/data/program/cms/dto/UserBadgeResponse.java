874d8a3915d94686b41a18aee644c3d0503ebd95
nhunh <nhunh@earable.ai>
2022-08-08 23:56:51 +0700

    add code

---
+++ data/src/main/java/ai/earable/platform/common/data/program/cms/dto/UserBadgeResponse.java
@@ -0,0 +1,25 @@
+package ai.earable.platform.common.data.program.cms.dto;
+
+import ai.earable.platform.common.data.program.common.enums.ProgramType;
+import ai.earable.platform.common.data.program.common.model.Badge;
+import ai.earable.platform.common.data.program.user.enums.UserBadgeStatus;
+import lombok.*;
+
+import java.util.UUID;
+
+/**
+ * Created by NhuNH on 08/08/2022
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@With
+public class UserBadgeResponse extends Badge {
+    private UUID badgeId;
+    private UUID userId;
+    private UUID programId;
+    private ProgramType programType;
+    private UserBadgeStatus status;
+    protected Long receivedAt;
+}
