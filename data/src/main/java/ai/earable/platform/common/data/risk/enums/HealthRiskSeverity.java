6d7d034c79d211ef13adfe848bb8a600ea489357
BinhNH <binhnh@earable.ai>
2022-08-09 13:00:34 +0700

    refactor severity comparing

--- data/src/main/java/ai/earable/platform/common/data/risk/enums/HealthRiskSeverity.java
+++ data/src/main/java/ai/earable/platform/common/data/risk/enums/HealthRiskSeverity.java
@@ -8,7 +8,7 @@ public enum HealthRiskSeverity {
     EMERGENCY(3);
 
     @Getter
-    private int priority;
+    private final int priority;
 
     HealthRiskSeverity(int priority) {
         this.priority = priority;
@@ -18,13 +18,6 @@ public enum HealthRiskSeverity {
      * Compare priority of status ( 0 = equal, 1 is target greater than source, -1 is target less than source )
      */
     public static int comparePriority(HealthRiskSeverity source, HealthRiskSeverity target) {
-        if (target.priority == source.priority) {
-            return 0;
-        }
-        if (target.priority > source.priority) {
-            return 1;
-        } else {
-            return -1;
-        }
+        return Integer.compare(target.priority, source.priority);
     }
 }
