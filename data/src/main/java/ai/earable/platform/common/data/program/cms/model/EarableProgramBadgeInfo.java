3631abcdac0ce1b9456e1829001e008d7287d792
nhunh <nhunh@earable.ai>
2022-08-09 22:24:16 +0700

    fix bug

--- data/src/main/java/ai/earable/platform/common/data/program/cms/model/EarableProgramBadgeInfo.java
+++ data/src/main/java/ai/earable/platform/common/data/program/cms/model/EarableProgramBadgeInfo.java
@@ -1,8 +1,11 @@
 package ai.earable.platform.common.data.program.cms.model;
 
+import ai.earable.platform.common.data.program.common.enums.BenefitType;
 import ai.earable.platform.common.data.program.common.enums.Receiver;
+import ai.earable.platform.common.data.program.common.enums.SessionPoint;
 import ai.earable.platform.common.data.program.common.model.Badge;
 import lombok.*;
+import org.springframework.data.cassandra.core.mapping.Column;
 import org.springframework.data.cassandra.core.mapping.UserDefinedType;
 
 import java.util.UUID;
@@ -13,7 +16,19 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @With
-public class EarableProgramBadgeInfo extends Badge {
+public class EarableProgramBadgeInfo {
     private UUID badgeId;
     private Receiver receiver;
+    protected String title;
+    protected String description;
+    protected String icon;
+    @Column("benefit_value")
+    protected Double benefitValue;
+    @Column("benefit_type")
+    protected BenefitType benefitType;
+    @Column("session_point")
+    protected SessionPoint sessionPoint;
+    @Column("benefit_effective_period")
+    protected Integer benefitEffectivePeriod;
+    protected Long createAt;
 }
