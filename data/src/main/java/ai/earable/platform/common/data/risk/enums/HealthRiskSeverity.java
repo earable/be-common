3f204eb5ac645dca08d6a9d41d64c4322d8e87af
BinhNH <binhnh@earable.ai>
2022-08-09 12:59:31 +0700

    update Health risk severity

--- data/src/main/java/ai/earable/platform/common/data/risk/enums/HealthRiskSeverity.java
+++ data/src/main/java/ai/earable/platform/common/data/risk/enums/HealthRiskSeverity.java
@@ -1,7 +1,30 @@
 package ai.earable.platform.common.data.risk.enums;
 
+import lombok.Getter;
+
 public enum HealthRiskSeverity {
-    NORMAL,
-    WARNING,
-    EMERGENCY
+    NORMAL(1),
+    WARNING(2),
+    EMERGENCY(3);
+
+    @Getter
+    private int priority;
+
+    HealthRiskSeverity(int priority) {
+        this.priority = priority;
+    }
+
+    /**
+     * Compare priority of status ( 0 = equal, 1 is target greater than source, -1 is target less than source )
+     */
+    public static int comparePriority(HealthRiskSeverity source, HealthRiskSeverity target) {
+        if (target.priority == source.priority) {
+            return 0;
+        }
+        if (target.priority > source.priority) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
 }
