fd910f6a60cbef5ad81504548d3095101f1c1f1a
BinhNH <binhnh@earable.ai>
2022-08-10 13:33:56 +0700

    update

--- data/src/main/java/ai/earable/platform/common/data/fcm/dto/DeleteFcmTokenRequest.java
+++ data/src/main/java/ai/earable/platform/common/data/fcm/dto/DeleteFcmTokenRequest.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import java.util.UUID;
 
 /**
@@ -19,6 +20,6 @@ import java.util.UUID;
 public class DeleteFcmTokenRequest {
     @NotBlank(message = "deviceId is required")
     private String deviceId;
-    @NotBlank(message = "userId is required")
-    private String userId;
+    @NotEmpty(message = "userId is required")
+    private UUID userId;
 }
