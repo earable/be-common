f550e946e34b138ea48e1089a7edd9397a118a71
BinhNH <binhnh@earable.ai>
2022-08-19 12:28:03 +0700

    add session id to EndSessionReq

--- data/src/main/java/ai/earable/platform/common/data/program/user/dto/EndSessionRequest.java
+++ data/src/main/java/ai/earable/platform/common/data/program/user/dto/EndSessionRequest.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class EndSessionRequest {
+    private String sessionId;
     private ActivityType activityType;
     private Long startTime;
     private Long endTime;
@@ -25,6 +26,7 @@ public class EndSessionRequest {
     private Integer year;
 
     public void validate() {
+        ValidationUtils.checkNull("sessionId", this.sessionId);
         ValidationUtils.checkNull("ActivityType", this.activityType);
         ValidationUtils.checkNull("startTime", this.startTime);
         ValidationUtils.checkNull("sessionPoint", this.sessionPoint);
