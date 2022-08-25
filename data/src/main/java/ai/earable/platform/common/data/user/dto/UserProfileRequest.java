54329caf0d900f99d42948161f26b63551896e48
BinhNH <binhnh@earable.ai>
2022-08-25 16:26:44 +0700

    add UserProfileResponse DTO

---
+++ data/src/main/java/ai/earable/platform/common/data/user/dto/UserProfileRequest.java
@@ -0,0 +1,34 @@
+package ai.earable.platform.common.data.user.dto;
+
+import ai.earable.platform.common.data.user.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+
+/**
+ * @author Hungnv
+ * @date 02/06/2022
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserProfileRequest {
+    @Size(max = 255, message = "name must not be greater than 255 characters.")
+    private String name;
+    @Size(min = 6, max = 20, message = "phone must be on range from 6 to 20 characters.")
+    private String phone;
+    private String address;
+    @Size(max = 255, message = "occupation must not be greater than 255 characters.")
+    private String occupation;
+    private String dob;
+    @Min(value = 1, message = "height must be greater than or equal to 1")
+    private Integer height;
+    @Min(value = 1, message = "weight must be greater than or equal to 1")
+    private Integer weight;
+    private Gender gender;
+}
---
+++ data/src/main/java/ai/earable/platform/common/data/user/dto/UserProfileResponse.java
@@ -0,0 +1,46 @@
+package ai.earable.platform.common.data.user.dto;
+
+import ai.earable.platform.common.data.user.enums.Gender;
+import ai.earable.platform.common.data.user.enums.ProfileProgress;
+import ai.earable.platform.common.data.user.enums.UserLevel;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import java.util.UUID;
+
+/**
+ * @author Hungnv
+ * @date 02/06/2022
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@With
+public class UserProfileResponse {
+    private UUID userId;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+    private String occupation;
+    private String dob;
+    private Integer height;
+    private Integer weight;
+    private Gender gender;
+    private String avatar;
+    private UserLevel userLevel;
+    private Double score;
+    private Double nextLevelScore;
+    private UUID programId;
+    private UUID badgeId;
+    private String badgeIcon;
+    private String badgeTitle;
+    private Long dailyWear;
+    private Double focusHours;
+    private Double recoveryHours;
+    private Long currentStreak;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Long ranking;
+    private ProfileProgress profileProgress;
+}
