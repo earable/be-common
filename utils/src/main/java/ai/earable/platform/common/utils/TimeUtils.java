24bdf0ed671063db66b3477421c4bc40dfbf54cf
manhdd <manhdd1991@gmail.com>
2022-09-14 12:42:22 +0700

    update code compare Time

--- utils/src/main/java/ai/earable/platform/common/utils/TimeUtils.java
+++ utils/src/main/java/ai/earable/platform/common/utils/TimeUtils.java
@@ -8,10 +8,15 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
 
+import static ai.earable.platform.common.utils.TimeUtils.isLessThanOrEqual;
+
 /**
  * Created by BinhNH on 3/28/2022
  */
+
+
 public final class TimeUtils {
+
     public static final String VN_TIME_ZONE_STRING = "Asia/Ho_Chi_Minh";
     public static final String AMERICA_TZ_STRING = "America/New_York";
 
@@ -47,28 +52,23 @@ public final class TimeUtils {
     }
 
     public static boolean isGreaterThan(LocalTime input, LocalTime compareTo){
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("hh:mm a", Locale.ENGLISH);
-        return input.format(dtf).compareTo(compareTo.format(dtf)) > 0;
+        return input.compareTo(compareTo) > 0;
     }
 
     public static boolean isEqual(LocalTime input, LocalTime compareTo){
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("hh:mm a", Locale.ENGLISH);
-        return input.format(dtf).compareTo(compareTo.format(dtf)) == 0;
+        return input.compareTo(compareTo) == 0;
     }
 
     public static boolean isLessThan(LocalTime input, LocalTime compareTo){
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("hh:mm a", Locale.ENGLISH);
-        return input.format(dtf).compareTo(compareTo.format(dtf)) < 0;
+        return input.compareTo(compareTo) < 0;
     }
 
     public static boolean isLessThanOrEqual(LocalTime input, LocalTime compareTo){
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("hh:mm a", Locale.ENGLISH);
-        return input.format(dtf).compareTo(compareTo.format(dtf)) <= 0;
+        return input.compareTo(compareTo) <= 0;
     }
 
     public static boolean isGreaterThanOrEqual(LocalTime input, LocalTime compareTo){
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("hh:mm a", Locale.ENGLISH);
-        return input.format(dtf).compareTo(compareTo.format(dtf)) >= 0;
+        return input.compareTo(compareTo) >= 0;
     }
 
     public static int getHourFromTimestamp(long timestamp, String timezone){
@@ -82,10 +82,9 @@ public final class TimeUtils {
     }
 
     public static void main(String[] args) {
-        System.out.println(getNumberOfSecondsBetween(1657702497, 1657702527));
-        System.out.println(getCurrentUnixTimestamp(AMERICA_TZ_STRING) + "\n" + getNextTimestamp(AMERICA_TZ_STRING, 3));
-        System.out.println(getZoneOffset(System.currentTimeMillis(), AMERICA_TZ_STRING));
-        System.out.println(convertFromUnixTimestamp(getNextTimestamp(AMERICA_TZ_STRING, 180)));
-        System.out.println(getHourFromTimestamp(1662663427000L, VN_TIME_ZONE_STRING));
+
+        LocalTime inputEnd = LocalTime.of(11, 01);
+        LocalTime existedEnd = LocalTime.of(11, 01);
+        System.out.println(isEqual(inputEnd, existedEnd));
     }
 }
