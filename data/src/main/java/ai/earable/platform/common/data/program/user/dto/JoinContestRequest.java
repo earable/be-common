86fb4a6279d91c65e2fcb45d58d9f8264b46db85
Nguyen Duy Thao <thaond@earable.ai>
2022-10-13 16:28:56 +0700

    Update user program state and request

--- data/src/main/java/ai/earable/platform/common/data/program/user/dto/JoinContestRequest.java
+++ data/src/main/java/ai/earable/platform/common/data/program/user/dto/JoinContestRequest.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class JoinContestRequest {
-    private String contestId;
+    private UUID contestId;
     private String confirm;
 }
--- data/src/main/java/ai/earable/platform/common/data/program/user/enums/UserProgramState.java
+++ data/src/main/java/ai/earable/platform/common/data/program/user/enums/UserProgramState.java
@@ -1,7 +1,16 @@
 package ai.earable.platform.common.data.program.user.enums;
 
 public enum UserProgramState {
-    INCOMPLETE,
-    COMPLETED,
-    CANCELLED
+
+    JOIN,         /* When the user starts joining a program */
+    DOING,        /* When the user begins doing a program   */
+    INCOMPLETE,   /* When user program do not invalid       */
+    COMPLETED,    /* When user did complete a program       */
+    CANCEL        /* When user cancel the program           */
+
+    /*
+    * On-going: when user_program_state = DOING
+    * Completed: when user_program_state = COMPLETED
+    * Upcoming: when get latest program from ABS and ignore program_id status = JOIN | DOING | INCOMPLETE | COMPLETED.
+    * */
 }
