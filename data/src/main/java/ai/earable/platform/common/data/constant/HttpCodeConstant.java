24b0a6fad5e20fa18be8da34613f9555d95c1bd0
dungntit <>
2023-07-07 17:10:01 +0700

    Add feature to Release Version (Android/IOS/Firmware) by CMS

---
+++ data/src/main/java/ai/earable/platform/common/data/constant/HttpCodeConstant.java
@@ -0,0 +1,9 @@
+package ai.earable.platform.common.data.constant;
+
+public class HttpCodeConstant {
+    public static final Integer BAD_REQUEST = 400;
+    public static final Integer UNAUTHORIZED = 401;
+    public static final Integer FORBIDDEN = 403;
+    public static final Integer NOT_FOUND = 404;
+    public static final Integer INTERNAL_ERROR = 500;
+}
