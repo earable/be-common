6ef4b2cd0e6a1eb1d47e96a63427ab4eac3be4ad
dungnt-earable <110391700+dungnt-earable@users.noreply.github.com>
2023-09-14 17:53:22 +0700

    add common method calculateVicQueryWindow

--- utils/src/main/java/ai/earable/platform/common/utils/Utils.java
+++ utils/src/main/java/ai/earable/platform/common/utils/Utils.java
@@ -1,11 +1,13 @@
 package ai.earable.platform.common.utils;
 
+import ai.earable.platform.common.data.session.model.Session;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.server.ServerWebExchange;
 
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author DungNT
@@ -24,4 +26,15 @@ public class Utils {
             return UUID.randomUUID().toString();
         }
     }
+    public static String calculateVicQueryWindow(Session session) {
+        if (session.getClientTimestamp() > 0) {
+            long daysDiff =
+                    TimeUnit.DAYS.convert(System.currentTimeMillis() - session.getClientTimestamp(), TimeUnit.MILLISECONDS);
+            String window = (daysDiff + 2) + "d";
+            return window;
+        } else {
+            return "1d";
+        }
+    }
+
 }
