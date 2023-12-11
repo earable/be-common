f5118297f8de3b6b6be6ae1e01b9ef3a624f0133
dungnt-earable <110391700+dungnt-earable@users.noreply.github.com>
2023-12-11 10:14:25 +0000

    Add metaDataSetting Field for table user_profile & entity class

--- data/src/main/java/ai/earable/platform/common/data/user/dto/UserProfileRequest.java
+++ data/src/main/java/ai/earable/platform/common/data/user/dto/UserProfileRequest.java
@@ -10,6 +10,7 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+import java.util.Map;
 
 /**
  * @author Hungnv
@@ -36,4 +37,5 @@ public class UserProfileRequest {
     private Gender gender;
     private String gender_1;
     private String restoreId;
+    private Map<String, String> metaDataSetting;
 }
