4287b10f6d0a2cc828032ddc681caba1605f0b53
dungntit <>
2024-03-25 21:59:57 +0700

    this is commit 6

--- data/src/main/java/ai/earable/platform/common/data/TestCICD.java
+++ data/src/main/java/ai/earable/platform/common/data/TestCICD.java
@@ -3,6 +3,7 @@ package ai.earable.platform.common.data;
 public class TestCICD {
     public static String getBranch() {
         // this is commit 5
+        // this is commit 6
         return "dev";
     }
 }
