1d1a0273f5a23b6e866b859c23acf8bd66c09970
Tung Pham <ml1@earable.ai>
2024-09-30 13:39:55 +0700

    update new purpose

--- data/src/main/java/ai/earable/platform/common/data/constant/AudioPurpose.java
+++ data/src/main/java/ai/earable/platform/common/data/constant/AudioPurpose.java
@@ -13,7 +13,22 @@ public enum AudioPurpose {
     BACKGROUND_BACK_TO_SLEEP("Background (Back to sleep)"),
     BACKGROUND_SPECIAL_SLEEP("Background (Special sleep)"),
     VOICE_COACH_SPECIAL_SLEEP("Voice coach (Special sleep)"),
-    VOICE_COACH_V3_SLEEP_PREFACE("Voice coaching Preface (Sleep_V3.0)");
+    
+    VOICE_COACH_V3_SLEEP_PREFACE("Voice coaching Preface (Sleep_V3.0)"),
+    VOICE_COACH_V3_SLEEP_COGNITIVE_RESTRUCTURING("Voice coaching Cognitive Restructuring (Sleep_V3.0)"),
+    VOICE_COACH_V3_SLEEP_BREATH_COACHING("Voice coaching Breath Coaching (Sleep_V3.0)"),
+    VOICE_COACH_V3_SLEEP_RELAXATION_TRAINING("Voice coaching Relaxation Training (Sleep_V3.0)"),
+    VOICE_COACH_V3_SLEEP_INFINITE_LOOP("Voice coaching Infinite Loop (Sleep_V3.0)"),
+    VOICE_COACH_V3_NAP_PREFACE("Voice coaching Preface (Nap_V3.0)"),
+    VOICE_COACH_V3_NAP_COGNITIVE_RESTRUCTURING("Voice coaching Cognitive Restructuring (Nap_V3.0)"),
+    VOICE_COACH_V3_NAP_BREATH_COACHING("Voice coaching Breath Coaching (Nap_V3.0)"),
+    VOICE_COACH_V3_NAP_RELAXATION_TRAINING("Voice coaching Relaxation Training (Nap_V3.0)"),
+    VOICE_COACH_V3_NAP_INFINITE_LOOP("Voice coaching Infinite Loop (Nap_V3.0)"),
+    VOICE_COACH_V3_BT_SLEEP_PREFACE("Voice coaching Preface (BackToSleep_V3.0)"),
+    VOICE_COACH_V3_BT_SLEEP_COGNITIVE_RESTRUCTURING("Voice coaching Cognitive Restructuring (BackToSleep_V3.0)"),
+    VOICE_COACH_V3_BT_SLEEP_BREATH_COACHING("Voice coaching Breath Coaching (BackToSleep_V3.0)"),
+    VOICE_COACH_V3_BT_SLEEP_RELAXATION_TRAINING("Voice coaching Relaxation Training (BackToSleep_V3.0)"),
+    VOICE_COACH_V3_BT_SLEEP_INFINITE_LOOP("Voice coaching Infinite Loop (BackToSleep_V3.0)");
     
 
     private final String label;
